96e1c5278a7c8616252ceac43341f3e3ad48419a
Gayan Malinda <73253202+GayanMalinda99@users.noreply.github.com>
2021-08-22 21:01:39 +0530

    v2 final code

--- src/com/kelaniya/uni/v2/Main.java
+++ src/com/kelaniya/uni/v2/Main.java
@@ -1,8 +1,14 @@
 package com.kelaniya.uni.v2;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+//SOLID PRINCIPLES -->> ALL SOLID PRINCIPLES ARE VIOLATED
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
 
         //Make sure to validate the arguments before using...
         if(args.length == 0){
@@ -19,7 +25,25 @@ public class Main {
             return;
         }
 
+        //Read the numbers text line
+        List<String> numbersStrs = Files.readAllLines(
+                Paths.get("C:/Users/DELL inspiron/Documents/GitHub/Software Construction/Calculator/numbers.txt")
+        );
 
-        System.out.println(args[0]);
+        double number1 = Double.parseDouble(numbersStrs.get(0));
+        double number2 = Double.parseDouble(numbersStrs.get(1));
+
+        double result = 0;
+
+        // Or use SWITCH CASE
+        if (operator.equals("add")){
+            result = number1 + number2;
+        } else if (operator.equals("sub")){
+            result = number1 - number2;
+        } else if (operator.equals("mul")){
+            result = number1 * number2;
+        }
+
+        System.out.println("The result is " + result);
     }
 }
