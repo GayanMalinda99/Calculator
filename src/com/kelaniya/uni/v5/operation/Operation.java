368951205518b2046a8463166c561c7eb8747f61
Gayan Malinda <73253202+GayanMalinda99@users.noreply.github.com>
2021-09-12 17:33:35 +0530

    Add v5 - Applying Dependency Inversion Principle

---
+++ src/com/kelaniya/uni/v5/operation/Operation.java
@@ -0,0 +1,8 @@
+package com.kelaniya.uni.v5.operation;
+
+//Abstract class (public abstract Operation()) Or Interface Class (public interface Operation())
+public interface Operation {
+
+    Double execute(Double[] numbers);
+
+}
